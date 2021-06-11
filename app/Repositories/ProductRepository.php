a180299b8779dcace347507e052cdce7320a8966
frog <tandoan0912@gmail.com>
2021-06-11 18:58:29 +0700

    tan

--- app/Repositories/ProductRepository.php
+++ app/Repositories/ProductRepository.php
@@ -170,7 +170,7 @@ class ProductRepository extends AbstractRepository {
         $category_id = DB::table('category')->where('alias',$category)->pluck('id');
         foreach($category_id as $category)
         $product_id = DB::table('product_category')->where('category_id',$category)->pluck('product_id');
-        $value = $this->model->where('status', 1)->whereIn('id',$product_id)->orderBy('created_at', 'desc')->take($limit)->paginate(2);
+        $value = $this->model->where('status', 1)->whereIn('id',$product_id)->orderBy('created_at', 'desc')->take($limit)->paginate(5);
         return $value;
     }
 
@@ -233,7 +233,7 @@ class ProductRepository extends AbstractRepository {
             $id = DB::table('product_category')->where('category_id',$input['location'])->pluck('product_id');
             $query->whereIn('id', $id);
         }
-        $records = $query->paginate(2);
+        $records = $query->paginate(5);
         return $records;
     }
 
